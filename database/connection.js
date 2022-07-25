c9f15791bd5bf03cb0344a188befbd18e071b66b
Usama Ahmed <usama.ahmed@emerald-labs.com>
2022-07-25 18:17:52 +0500

    Changes made to the connection string in connection.js

--- database/connection.js
+++ database/connection.js
@@ -5,7 +5,7 @@ const Sequelize = require("sequelize");
 if (process.env.NODE_ENV === "production") {
   const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
   var pool = new Pool({
-    connectionString: isProduction
+    connectionString: "production"
       ? process.env.DATABASE_URL
       : connectionString,
     ssl: {
